720c851f6f613eff033a665b77602ca676c9d0bc
U-DESKTOP-URLHABG\Thomas <thomasmaters@hotmail.com>
2018-07-24 09:05:32 +0200

    Fixed floating point calculation misstake. Added calculating dune area. Refractord some code.

--- SensorInterface/src/Dune.hpp
+++ SensorInterface/src/Dune.hpp
@@ -11,28 +11,28 @@
 
 #include "Matrix.hpp"
 
-#include <vector>
 #include <string>
+#include <vector>
 
 struct Dune
 {
-	std::size_t start_;
-	std::size_t size_;
-	Matrix<4,1,float> signature_;
-	float area_;
+    std::size_t start_;
+    std::size_t size_;
+    Matrix<4, 1, float> signature_;
+    float area_;
 
-	std::string toString()
-	{
-		return "start: " + std::to_string(start_) + " size: " + std::to_string(size_) + " area:" + std::to_string(area_);
-	}
+    std::string toString()
+    {
+        return "start: " + std::to_string(start_) + " size: " + std::to_string(size_) + " area:" +
+               std::to_string(area_);
+    }
 };
 
-template<std::size_t H, std::size_t W, typename T>
+template <std::size_t H, std::size_t W, typename T>
 struct BottomProfile
 {
-	Matrix<H,W,T> raw_data_;
-	std::vector<Dune> dunes_;
+    Matrix<H, W, T> raw_data_;
+    std::vector<Dune> dunes_;
 };
-
 
 #endif /* SRC_DUNE_HPP_ */

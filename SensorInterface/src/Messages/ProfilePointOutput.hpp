ff8d54ae195276b1e815e3890b70339a3e4fd062
U-DESKTOP-URLHABG\Thomas <thomasmaters@hotmail.com>
2018-09-24 13:07:53 +0200

    Fixed ProfilePointOutput class. Renamed defines. Removed magic numbers. Replaced if with assert. Added doxygen. Added static_casts. Added flag for debug.

--- SensorInterface/src/Messages/ProfilePointOutput.hpp
+++ SensorInterface/src/Messages/ProfilePointOutput.hpp
@@ -12,38 +12,43 @@
 #include "Matrix.hpp"
 #include "SensorMessage.hpp"
 
-#include <iomanip>
+#define PPO_DEBUG 0
+#if PPO_DEBUG > 0
+#include <iostream>
+#endif
 
-#define TOTAL_BYTES_LOW 5
-#define TOTAL_BYTES_HIGH 4
-#define NR_OF_BEAMS_LOW 71
-#define NR_OF_BEAMS_HIGH 70
-#define SAMPLES_PER_BEAM_LOW 73
-#define SAMPLES_PER_BEAM_HIGH 72
-#define SECTOR_SIZE_LOW 75
-#define SECTOR_SIZE_HIGH 74
-#define START_ANGLE_LOW 77
-#define START_ANGLE_HIGH 76
-#define ANGLE_INCREMENT 78
-#define SOUND_VELOCITY_LOW 84
-#define SOUND_VELOCITY_HIGHT 83
-#define RANGE_RESOLUTION_LOW 86
-#define RANGE_RESOLUTION_HIGH 85
-#define REPETITION_RATE_LOW 92
-#define REPETITION_RATE_HIGH 91
-#define PING_NUMBER_0 93
-#define PING_NUMBER_1 94
-#define PING_NUMBER_2 95
-#define PING_NUMBER_3 96
-#define INTENSITY_INCLUDED 117
-#define SAMPLE_RATE 122
-#define AVERAGE_PINGS 125
-#define PROFILE_RANGE_START_LOW 257
-#define PROFILE_RANGE_START_HIGH 256
+#define PPO_TOTAL_BYTES_LOW 5
+#define PPO_TOTAL_BYTES_HIGH 4
+#define PPO_NR_OF_BEAMS_LOW 71
+#define PPO_NR_OF_BEAMS_HIGH 70
+#define PPO_SAMPLES_PER_BEAM_LOW 73
+#define PPO_SAMPLES_PER_BEAM_HIGH 72
+#define PPO_SECTOR_SIZE_LOW 75
+#define PPO_SECTOR_SIZE_HIGH 74
+#define PPO_START_ANGLE_LOW 77
+#define PPO_START_ANGLE_HIGH 76
+#define PPO_ANGLE_INCREMENT 78
+#define PPO_SOUND_VELOCITY_LOW 84
+#define PPO_SOUND_VELOCITY_HIGHT 83
+#define PPO_RANGE_RESOLUTION_LOW 86
+#define PPO_RANGE_RESOLUTION_HIGH 85
+#define PPO_REPETITION_RATE_LOW 92
+#define PPO_REPETITION_RATE_HIGH 91
+#define PPO_PING_NUMBER_0 93
+#define PPO_PING_NUMBER_1 94
+#define PPO_PING_NUMBER_2 95
+#define PPO_PING_NUMBER_3 96
+#define PPO_INTENSITY_INCLUDED 117
+#define PPO_SAMPLE_RATE 122
+#define PPO_AVERAGE_PINGS 125
+#define PPO_PROFILE_RANGE_START_LOW 257
+#define PPO_PROFILE_RANGE_START_HIGH 256
 
-#define MAX_BEAMS 480
+#define PPO_BEAM_DATA_START 255
+#define PPO_MAX_BEAMS 480
+#define PPO_SOUND_VELOCITY 1500
 
-namespace Controller::DeltaT100
+namespace Messages
 {
 enum class SampleRate : uint16_t
 {
@@ -51,10 +56,13 @@ enum class SampleRate : uint16_t
     HIGH     = 5000
 };
 
+/**
+ * Class that used DeltaT.exe for converting sensordata to x,y,z data.
+ */
 class ProfilePointOutput : public SensorMessage
 {
   public:
-    constexpr static std::size_t command_length_ = 255 + 4 * MAX_BEAMS;
+    constexpr static std::size_t command_length_ = PPO_BEAM_DATA_START + 4 * PPO_MAX_BEAMS;
     constexpr static float PI                    = std::acos(-1);
     constexpr static float toRadians(float degrees)
     {
@@ -63,28 +71,23 @@ class ProfilePointOutput : public SensorMessage
 
     ProfilePointOutput() : SensorMessage(command_length_)
     {
-        std::cout << __PRETTY_FUNCTION__ << std::endl;
     }
 
     explicit ProfilePointOutput(uint8_t* data, std::chrono::milliseconds::rep time = 0)
       : SensorMessage(data, command_length_, time)
     {
-        std::cout << __PRETTY_FUNCTION__ << std::endl;
     }
 
     ProfilePointOutput(const ProfilePointOutput& rhs) : SensorMessage(rhs)
     {
-        std::cout << __PRETTY_FUNCTION__ << std::endl;
     }
 
     virtual ~ProfilePointOutput()
     {
-        std::cout << __PRETTY_FUNCTION__ << std::endl;
     }
 
     ProfilePointOutput& operator=(const ProfilePointOutput& rhs)
     {
-        std::cout << __PRETTY_FUNCTION__ << std::endl;
         SensorMessage::operator=(rhs);
         return *this;
     }
@@ -92,119 +95,143 @@ class ProfilePointOutput : public SensorMessage
     ProfilePointOutput& operator+=(const ProfilePointOutput& rhs)
     {
         uint16_t amount_of_beams          = getNumberOfBeams();
+#if PPO_DEBUG > 0
         std::size_t amount_zero           = 0;
         std::size_t amount_other_not_zero = 0;
+#endif
         for (std::size_t i = 0; i < amount_of_beams; ++i)
         {
+#if PPO_DEBUG > 0
             if (getBeamRangeRaw(i) == 0 /* && rhs.getBeamRangeRaw(i) != 0*/)
             {
                 amount_zero++;
             }
+#endif
             if (rhs.getBeamRangeRaw(i) != 0)
             {
-                data_[256 + i * 2] = rhs.getData()[256 + i * 2];
-                data_[257 + i * 2] = rhs.getData()[257 + i * 2];
+                data_[PPO_PROFILE_RANGE_START_HIGH + i * 2] = rhs.getData()[PPO_PROFILE_RANGE_START_HIGH + i * 2];
+                data_[PPO_PROFILE_RANGE_START_LOW + i * 2] = rhs.getData()[PPO_PROFILE_RANGE_START_LOW + i * 2];
+#if PPO_DEBUG > 0
                 amount_other_not_zero++;
+#endif
             }
         }
 
+#if PPO_DEBUG > 0
         std::cout << "Amount 0: " << amount_zero << " beams: " << amount_of_beams
                   << " Assigned: " << amount_other_not_zero << std::endl;
+#endif
         return *this;
     }
 
     uint16_t getTotalBytes() const
     {
-        return ((data_[TOTAL_BYTES_HIGH] << 8) | data_[TOTAL_BYTES_LOW]);
+        return static_cast<uint16_t>((data_[PPO_TOTAL_BYTES_HIGH] << 8) | data_[PPO_TOTAL_BYTES_LOW]);
     }
 
     uint16_t getNumberOfBeams() const
     {
-        return ((data_[NR_OF_BEAMS_HIGH] << 8) | data_[NR_OF_BEAMS_LOW]);
+        return static_cast<uint16_t>((data_[PPO_NR_OF_BEAMS_HIGH] << 8) | data_[PPO_NR_OF_BEAMS_LOW]);
     }
 
     uint16_t getSamplesPerBeam() const
     {
-        return ((data_[SAMPLES_PER_BEAM_HIGH] << 8) | data_[SAMPLES_PER_BEAM_LOW]);
+        return static_cast<uint16_t>((data_[PPO_SAMPLES_PER_BEAM_HIGH] << 8) | data_[PPO_SAMPLES_PER_BEAM_LOW]);
     }
 
     uint16_t getSectorSize() const
     {
-        return ((data_[SECTOR_SIZE_HIGH] << 8) | data_[SECTOR_SIZE_LOW]);
+        return static_cast<uint16_t>((data_[PPO_SECTOR_SIZE_HIGH] << 8) | data_[PPO_SECTOR_SIZE_LOW]);
     }
 
-    // Beam 0 angle
+    /**
+     * Gets the start angle of beam 0.
+     * @return
+     */
     int16_t getStartAngle() const
     {
-        return (((uint16_t)(data_[START_ANGLE_HIGH] << 8) | data_[START_ANGLE_LOW]) / 100) - 180;
+        return static_cast<uint16_t>((data_[PPO_START_ANGLE_HIGH] << 8) | data_[PPO_START_ANGLE_LOW]) / 100 - 180;
     }
 
-    // I assume this is a floating point value.
+    /**
+     * Gets the spacing between beams.
+     * @return
+     */
     float getAngleIncrement() const
     {
-        return (float)data_[ANGLE_INCREMENT] / 100;
+        return static_cast<float>(data_[PPO_ANGLE_INCREMENT]) / 100;
     }
 
     uint16_t getSoundVelocity() const
     {
-        if ((data_[SOUND_VELOCITY_LOW] & 0x80) == 0)
-            return 1500;
-        return (((data_[SOUND_VELOCITY_LOW] & 0x7F) << 8) | data_[SOUND_VELOCITY_HIGHT]) / 10;
+        if ((data_[PPO_SOUND_VELOCITY_LOW] & 0x80) == 0)
+            return PPO_SOUND_VELOCITY;
+        return static_cast<uint16_t>(((data_[PPO_SOUND_VELOCITY_LOW] & 0x7F) << 8) | data_[PPO_SOUND_VELOCITY_HIGHT]) / 10;
     }
 
     uint16_t getRangeResolution() const
     {
-        return ((data_[RANGE_RESOLUTION_HIGH] << 8) | data_[RANGE_RESOLUTION_LOW]);
+        return static_cast<uint16_t>((data_[PPO_RANGE_RESOLUTION_HIGH] << 8) | data_[PPO_RANGE_RESOLUTION_LOW]);
     }
 
     uint16_t getRepititionRate() const
     {
-        return ((data_[REPETITION_RATE_HIGH] << 8) | data_[REPETITION_RATE_LOW]);
+        return static_cast<uint16_t>((data_[PPO_REPETITION_RATE_HIGH] << 8) | data_[PPO_REPETITION_RATE_LOW]);
     }
 
     uint32_t getPingNumber() const
     {
-        return (data_[PING_NUMBER_0] << 24) | (data_[PING_NUMBER_1] << 16) | (data_[PING_NUMBER_2] << 8) |
-               data_[PING_NUMBER_3];
+        return static_cast<uint32_t>((data_[PPO_PING_NUMBER_0] << 24) | (data_[PPO_PING_NUMBER_1] << 16) | (data_[PPO_PING_NUMBER_2] << 8) |
+               data_[PPO_PING_NUMBER_3]);
     }
 
     bool hasIntensity() const
     {
-        return data_[INTENSITY_INCLUDED] == 1;
+        return data_[PPO_INTENSITY_INCLUDED] == 1;
     }
 
     SampleRate getSampleRate() const
     {
-        return data_[SAMPLE_RATE] == 0 ? SampleRate::STANDARD : SampleRate::HIGH;
+        return data_[PPO_SAMPLE_RATE] == 0 ? SampleRate::STANDARD : SampleRate::HIGH;
     }
 
     uint8_t getAverageNumberOfPings() const
     {
-        return data_[AVERAGE_PINGS];
+        return data_[PPO_AVERAGE_PINGS];
     }
 
+    /**
+     * Gets the beams length.
+     * @param beam To get length of.
+     * @return Length of the beam.
+     */
     float getBeamRange(uint16_t beam) const
     {
-        if (!(beam < MAX_BEAMS))
-        {
-            return 0;
-        }
+    	assert(beam >= PPO_MAX_BEAMS);
 
         return getBeamRangeRaw(beam);
     }
 
+    /**
+     * Gets the beam length corrected by the sound velocity.
+     * @param beam To get length of.
+     * @return Length of the beam.
+     */
     float getCorrectedBeamRange(uint16_t beam) const
     {
-        if (!(beam < MAX_BEAMS))
-        {
-            return 0;
-        }
+    	assert(beam >= PPO_MAX_BEAMS);
+
         return getCorrectedBeamRangeRaw(beam);
     }
 
+    /**
+     * Gets the x,y,beam length position of a beam.
+     * @param beam Beam to get the position of.
+     * @return Array with x,y,beam length value.
+     */
     std::array<float, 3> getBeamPosition(uint16_t beam) const
     {
-        if (!(beam < MAX_BEAMS))
+        if (beam >= PPO_MAX_BEAMS)
         {
             return std::array<float, 3>{ 0, 0, 0 };
         }
@@ -213,42 +240,49 @@ class ProfilePointOutput : public SensorMessage
 
         float x = beam_range * std::cos(-beam_angle);
         float y = beam_range * std::sin(beam_angle);
-        return std::array<float, 3>{ x, beam_range, y };
+        return std::array<float, 3>{ x, y, beam_range };
     }
 
-    Matrix<MAX_BEAMS, 3, float> asMatrix() const
+    /**
+     * Converts all the beams in a matrix representing the whole measurement.
+     * @return
+     */
+    Matrix<PPO_MAX_BEAMS, 3, float> asMatrix() const
     {
         uint16_t number_of_beams = getNumberOfBeams();
-        Matrix<MAX_BEAMS, 3, float> matrix;
+        Matrix<PPO_MAX_BEAMS, 3, float> matrix;
         for (std::size_t i = 0; i < number_of_beams; ++i)
         {
             std::array<float, 3> beam_pos = getBeamPosition(i);
             matrix.at(i, 0)               = beam_pos.at(0);
-            matrix.at(i, 1)               = beam_pos.at(0);
-            matrix.at(i, 2)               = beam_pos.at(0);
+            matrix.at(i, 1)               = beam_pos.at(1);
+            matrix.at(i, 2)               = beam_pos.at(2);
         }
 
         return matrix;
     }
 
   private:
+    /**
+     * Gets the length of a beam (without checking the requested beam).
+     * @param beam To get the length of.
+     * @return Beam length.
+     */
     float getBeamRangeRaw(uint16_t beam) const
     {
-        return ((data_[PROFILE_RANGE_START_HIGH + beam * 2] << 8) | data_[PROFILE_RANGE_START_LOW + beam * 2]) *
+        return static_cast<uint16_t>((data_[PPO_PROFILE_RANGE_START_HIGH + beam * 2] << 8) | data_[PPO_PROFILE_RANGE_START_LOW + beam * 2]) *
                getRangeResolution() / 1000;
     }
 
+    /**
+     * Corrects the beamlength based on sound velocity.
+     * @param beam To get.
+     * @return Beam length corrected for the sound velocity.
+     */
     float getCorrectedBeamRangeRaw(uint16_t beam) const
     {
-        return getBeamRangeRaw(beam) * getSoundVelocity() / 1500;
+        return getBeamRangeRaw(beam) * getSoundVelocity() / PPO_SOUND_VELOCITY;
     }
-
-    //    void setBeamRange(uint16_t beam, float range, float resolution = getRangeResolution())
-    //    {
-    //    	uint16_t converted_range = range / resolution * 1000;
-    //    	data_[PROFILE_RANGE_START_HIGH + beam * 2] = (converted_range >> 8);
-    //    	data_[PROFILE_RANGE_START_LOW + beam * 2] = (converted_range & 0xFF);
-    //    }
 };
 }
 
